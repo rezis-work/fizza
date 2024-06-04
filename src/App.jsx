fd56c4c69b10c890374328fcf785b4cc3af1f1c0
Rezi Karanadze <karanadzerevazi@gmail.com>
2024-06-05 00:52:19 +0400

    implement error handling for request actions

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,9 @@ import Home from "./ui/Home";
 import Menu, { Loader as menuLoader } from "./features/menu/Menu";
 import Cart from "./features/cart/Cart";
 import Order, { Loader as orderLoader } from "./features/order/Order";
-import CreateOrder from "./features/order/CreateOrder";
+import CreateOrder, {
+  Action as createOrderAction,
+} from "./features/order/CreateOrder";
 import AppLayout from "./ui/AppLayout";
 import Error from "./ui/Error";
 
@@ -35,6 +37,7 @@ const router = createBrowserRouter([
       {
         path: "/order/new",
         element: <CreateOrder />,
+        action: createOrderAction,
       },
     ],
   },
