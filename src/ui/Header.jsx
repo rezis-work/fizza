9d5a9851aa0fe18cb7cece06cd8b8fd78fdab9da
Rezi Karanadze <karanadzerevazi@gmail.com>
2024-06-04 17:43:44 +0400

    implement project layout

---
+++ src/ui/Header.jsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <header>
+      <Link to="/">Fizza Co.</Link>
+
+      <p>rezi karanadze</p>
+    </header>
+  );
+};
+
+export default Header;
