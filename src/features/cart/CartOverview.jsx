4e054aa18fd3fa838a9f8baaa6457a3109df7b66
Rezi Karanadze <karanadzerevazi@gmail.com>
2024-06-06 15:16:28 +0400

    implement cart oveview with redux states

--- src/features/cart/CartOverview.jsx
+++ src/features/cart/CartOverview.jsx
@@ -1,13 +1,32 @@
+import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
+import { getTotalCartPrice, getTotalCartQuantity } from "./cartSlice";
+import { formatCurrency } from "../../utils/helpers";
 
 function CartOverview() {
+  const totalCartQuantity = useSelector(getTotalCartQuantity);
+  const totalCartPrice = useSelector(getTotalCartPrice);
+  const user = useSelector((store) => store.user.username);
   return (
     <div className="flex items-center justify-between bg-stone-800 px-4 py-4 text-sm uppercase text-stone-200 sm:px-6 md:text-base">
-      <p className="space-x-4 font-semibold text-stone-300 sm:space-x-6">
-        <span>23 pizzas</span>
-        <span>$23.45</span>
-      </p>
-      <Link to="/cart">Open cart &rarr;</Link>
+      {!totalCartPrice && (
+        <p className="hidden items-center gap-2 md:flex">
+          <span>&copy; 2024 | Created by Rezi karanadze =&gt; </span> Pick your
+          pizza and grab to your cart {user}
+        </p>
+      )}
+      {!totalCartPrice && (
+        <p className="m-auto flex items-center gap-2 md:hidden">
+          <span>&copy; 2024 | Created by Rezi karanadze</span>
+        </p>
+      )}
+      {totalCartQuantity > 0 && (
+        <p className="space-x-4 font-semibold text-stone-300 sm:space-x-6">
+          <span>{totalCartQuantity} pizzas</span>
+          <span>{formatCurrency(totalCartPrice)}</span>
+        </p>
+      )}
+      {totalCartQuantity > 0 ? <Link to="/cart">Open cart &rarr;</Link> : ""}
     </div>
   );
 }
--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -1,17 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  // cart: [],
-
-  cart: [
-    {
-      pizzaId: 12,
-      name: "Mediterranean",
-      quantity: 2,
-      unitPrice: 16,
-      totalPrice: 32,
-    },
-  ],
+  cart: [],
 };
 
 const cartSlice = createSlice({
@@ -49,3 +39,9 @@ export const {
 } = cartSlice.actions;
 
 export default cartSlice.reducer;
+
+export const getTotalCartQuantity = (store) =>
+  store.cart.cart.reduce((sum, item) => sum + item.quantity, 0);
+
+export const getTotalCartPrice = (store) =>
+  store.cart.cart.reduce((sum, item) => sum + item.totalPrice, 0);
