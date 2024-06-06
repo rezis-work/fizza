2c74fae5c6525b0940f9a2864534c994d25c17ee
Rezi Karanadze <karanadzerevazi@gmail.com>
2024-06-06 12:06:45 +0400

    implement redux store & userSlice

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,6 +1,14 @@
+import { useSelector } from "react-redux";
+
 const Username = () => {
+  const username = useSelector((store) => store.user.username);
+
+  if (!username) return null;
+
   return (
-    <div className="hidden text-sm font-semibold uppercase md:block">Rezi</div>
+    <div className="hidden text-sm font-semibold uppercase md:block">
+      {username}
+    </div>
   );
 };
 
